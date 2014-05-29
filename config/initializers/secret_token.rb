b3cbdc5c76ef97bd0632e8586fd62b098debe5d0
SARATHKUMAR <sharathkumar@qburst.com>
2014-05-29 17:11:20 +0530

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IamWaiting::Application.config.secret_key_base = '69932ff5094e30173de793a4fbdcd25d2557fd49e9a692d9cf336f6c0e72c191fafc475d543ac8fba7c8e336b5976967f1fe6a69c3f953208cf3b778a09439e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IamWaiting::Application.config.session_store :cookie_store, key: '_iam_waiting_session'
